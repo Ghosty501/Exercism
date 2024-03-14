9b7b4b36177a270c88ad8a40e1753cefba645f21
Ghosty501 <0253260@up.edu.mx>
2024-03-13 23:33:04 -0600

    Actualizacion

--- Python/Currency Exchange.py
+++ Python/Currency Exchange.py
@@ -1,3 +1,12 @@
+"""Functions for calculating steps in exchaning currency.
+
+Python numbers documentation: https://docs.python.org/3/library/stdtypes.html#numeric-types-int-float-complex
+
+Overview of exchanging currency when travelling: https://www.compareremit.com/money-transfer-tips/guide-to-exchanging-currency-for-overseas-travel/
+"""
+
+
+
 def exchange_money(budget, exchange_rate):
     """
 
@@ -7,6 +16,7 @@ def exchange_money(budget, exchange_rate):
     """
     Exchange=budget/exchange_rate
     return Exchange
+
 
 def get_change(budget, exchanging_value):
     """
@@ -19,6 +29,7 @@ def get_change(budget, exchanging_value):
     Change=budget-exchanging_value
     return Change
 
+
 def get_value_of_bills(denomination, number_of_bills):
     """
 
@@ -29,6 +40,7 @@ def get_value_of_bills(denomination, number_of_bills):
 
     Value=denomination*number_of_bills
     return Value
+
 
 def get_number_of_bills(amount, denomination):
     """
@@ -41,3 +53,37 @@ def get_number_of_bills(amount, denomination):
     Number=amount//denomination
     return Number
 
+
+def get_leftover_of_bills(amount, denomination):
+    """
+
+    :param amount: float - the total starting value.
+    :param denomination: int - the value of a single bill.
+    :return: float - the amount that is "leftover", given the current denomination.
+    """
+
+    total4=amount%denomination
+    return total4
+
+
+def exchangeable_value(budget, exchange_rate, spread, denomination):
+    """
+
+    :param budget: float - the amount of your money you are planning to exchange.
+    :param exchange_rate: float - the unit value of the foreign currency.
+    :param spread: int - percentage that is taken as an exchange fee.
+    :param denomination: int - the value of a single bill.
+    :return: int - maximum value you can get.
+    """
+
+    # Convierte el diferencial a decimal
+    spread_decimal = (spread / 100) + 1
+
+    # Calcula el nuevo tipo de cambio
+    new_exchange_rate = exchange_rate * spread_decimal
+
+    # Calcula el valor máximo de la nueva moneda
+    max_value = budget / new_exchange_rate
+
+    # Devuelve el valor máximo como un entero (redondeado hacia abajo)
+    return int(max_value % denomination)
